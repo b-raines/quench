631c3ec16dd140c20c8b7634ce0444ed202d18b1
Brent Raines <bt.raines@gmail.com>
2014-06-24 09:23:26 -0400

    Added facebook feed

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,9 +1,8 @@
 $(function() {
     var about_slidr = slidr.create('slidr-img', {
         controls: 'none',
-        touch: 'true',
         keyboard: 'true',
         pause: 'true'
     }).auto();
-    about_slidr.add('h', ['one', 'two', 'three', 'four', 'one']);
+    about_slidr.add('h', ['one', 'two', 'three', 'four', 'five', 'one']);
 });
